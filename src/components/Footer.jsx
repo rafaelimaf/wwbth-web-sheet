68150b68a39133e247c7c607f746e0b637d574fe
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-25 14:23:27 -0300

    :art: Fix: Footer
    Fix footer link to redirect to a new tab and implement its CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="wwbth-footer">
-        <a href="https://github.com/rafaelimaf" className="footer-link-github">| Github</a>
+        <a href="https://github.com/rafaelimaf" target="_blank" className="footer-link-github" rel="noreferrer">| Github</a>
       </footer>
     )
   }
