45694851130e53ac44c82de87f1b30d106cf65a7
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-25 14:01:24 -0300

    :sparkles: Feat: Header
    Add a header and its CSS into the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import DescriptionSheet from '../components/DescriptionSheet'
 import AttributesSheet from '../components/AttributesSheet'
 import '../styles/Home.css'
 import VitalSheet from '../components/VitalSheet'
+import Header from '../components/Header'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="wwbth-sheets">
+        <Header />
         <div className="first-sheets">
           <DescriptionSheet />
           <div>
