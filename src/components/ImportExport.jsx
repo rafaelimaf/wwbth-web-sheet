9dc79652393c4fb1263fd8a09962d3599c2bac41
Moésio Filho <moesio.f@gmail.com>
2022-03-12 16:03:44 -0300

    Feat: adiciona export/import

---
+++ src/components/ImportExport.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import '../styles/ImportExport.css'
+
+export default class ImportExport extends Component {
+    readImport(event) {
+        let reader = new FileReader()
+        reader.onload = function(e) {
+            let data = JSON.parse(e.target.result)
+            Object.keys(data).forEach(function (k) { 
+                localStorage.setItem(k, data[k])
+            })
+            window.location.reload()
+        }
+        reader.readAsText(event.target.files[0])
+    }
+
+    downloadExport() {
+        let data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(localStorage));
+        let a = document.createElement('a');
+        a.setAttribute("href",     data);
+        a.setAttribute("download", 'data' + ".json");
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    }
+
+    render() {
+        return (
+            <div className="top-menu">
+                <button 
+                type="button"
+                id="export-button"
+                onClick={ this.downloadExport }/>
+                <button 
+                type="button"
+                id="import-button"
+                onClick={ () => { document.getElementById('file-picker').click() } }/>
+                <input 
+                type="file"
+                id="file-picker"
+                accept="application/json"
+                multiple={ false }
+                onChange={ this.readImport } 
+                hidden/>
+            </div>
+          )
+    }
+}
